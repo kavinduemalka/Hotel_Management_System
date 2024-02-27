624ff7f3740a660439a660248e52bd06d54d95a4
unknown <kavinduemalka1@gmail.com>
2024-02-27 22:07:24 +0530

    add new database

--- tables/HotelManagementSystem.sql
+++ tables/HotelManagementSystem.sql
@@ -36,6 +36,16 @@ CREATE TABLE `customers` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `customers`
+--
+
+LOCK TABLES `customers` WRITE;
+/*!40000 ALTER TABLE `customers` DISABLE KEYS */;
+INSERT INTO `customers` VALUES ('CS001','Pasan','Perera','pasan@gmail.com','712345656','No23,Araliya mawatha,Horana','M'),('CS009','Akila','Fonseka','akila123@gmail.com','765678897','No34,Ukwatta road,Ganemulla','M'),('CS012','Kasun','Samarasinghe','Kasun95@gmail.com','778912000','No75,Godigamuwa,Batuwita','M'),('CS020','Kanthi','Jayakody','jaya88@gmail.com','755523431','No234/2,Ella road,Maharagama','F'),('CS030','Malki','Herath','malkijayashani1996@gmail.com','770456450','No22,Vijaya mawatha,Kaduwela','F'),('CS035','Amal','Perera','amal675@gmail.com','789090345','No223,Hena mawatha,Kurunegala','M');
+/*!40000 ALTER TABLE `customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `reservations`
 --
 
@@ -60,6 +70,16 @@ CREATE TABLE `reservations` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+INSERT INTO `reservations` VALUES ('RS001','R001','CS001','2024-02-26','23:03:31',NULL,2000.00,1),('RS003','R045','CS012','2024-02-27','18:11:08',NULL,5000.00,0),('RS032','R062','CS020','2024-02-27','18:14:30',NULL,7500.00,0);
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `roomcategory`
 --
 
@@ -76,6 +96,16 @@ CREATE TABLE `roomcategory` (
   PRIMARY KEY (`CategoryID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roomcategory`
+--
+
+LOCK TABLES `roomcategory` WRITE;
+/*!40000 ALTER TABLE `roomcategory` DISABLE KEYS */;
+INSERT INTO `roomcategory` VALUES ('CT01','Single','Non A/C',1,2000.00,NULL),('CT02','Single','A/C',1,3500.00,NULL),('CT03','Double','Non A/C',2,3500.00,NULL),('CT04','Double','A/C',2,5000.00,NULL),('CT05','Triple','Non A/C',3,5000.00,NULL),('CT06','Triple','A/C',3,6500.00,NULL),('CT07','Family','Non A/C',5,7500.00,NULL),('CT08','Family','A/C',5,9000.00,NULL);
+/*!40000 ALTER TABLE `roomcategory` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `rooms`
@@ -97,6 +127,16 @@ CREATE TABLE `rooms` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `rooms`
+--
+
+LOCK TABLES `rooms` WRITE;
+/*!40000 ALTER TABLE `rooms` DISABLE KEYS */;
+INSERT INTO `rooms` VALUES ('R001','101','CT01','Available',NULL),('R011','111','CT02','Available',NULL),('R045','145','CT05','Available',NULL),('R062','162','CT07','Available',NULL),('R069','169','CT08','Available',NULL);
+/*!40000 ALTER TABLE `rooms` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -115,6 +155,16 @@ CREATE TABLE `user` (
   UNIQUE KEY `username` (`username`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Kavindu Emalka','970792345V','emalka@gmail.com','0772020201','Emalka','1234');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -125,4 +175,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-02-26 23:16:12
+-- Dump completed on 2024-02-27 21:49:44
